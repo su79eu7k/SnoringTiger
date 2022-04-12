90d5538598b3276b3a20cebe85cba662f6814485
sekim <sungeun.kim@zf.com>
2022-04-12 16:27:44 +0900

    buttons

--- src/pages/ConnectWorkbook.js
+++ src/pages/ConnectWorkbook.js
@@ -1,17 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Routes, Route, Outlet, Link } from "react-router-dom";
 import Typography from '@mui/material/Typography';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 
 export default function ConnectWorkbook() {
+  const [file, setFile] = useState()
+
+  function handleChange(e) {
+    setFile(e.target.files[0])
+    e.target.value = ''
+  }
+
   return (
     <>
       <Typography variant="h6">
         Connect Workbook
       </Typography>
       <Stack spacing={2} direction="row">
-        <Button variant="contained">Open File</Button>
-        <Button variant="contained">Next</Button>
+        <Button variant="contained" component="label">
+          File
+          <input type="file" onChange={handleChange} hidden />
+        </Button>
+        <Button
+          component={Link}
+          to="/assign_variables"
+          variant="contained">Next</Button>
       </Stack>
     </>
 
