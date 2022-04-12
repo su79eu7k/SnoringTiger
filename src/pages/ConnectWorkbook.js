9382d5eeaaf9aa4a27f2f0c2993c53c1c618ce61
sekim <sungeun.kim@zf.com>
2022-04-12 11:58:03 +0900

    Typography

--- src/pages/ConnectWorkbook.js
+++ src/pages/ConnectWorkbook.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 export default function ConnectWorkbook() {
   return (
-    <div>ConnectWorkbook</div>
+    <>
+      <Typography variant="h6">
+        Connect Workbook
+      </Typography>
+      <Stack spacing={2} direction="row">
+        <Button variant="contained">Open File</Button>
+        <Button variant="contained">Next</Button>
+      </Stack>
+    </>
+
   )
 }
