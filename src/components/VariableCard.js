e00af67b8752f840645c64d0629e86baa48f80b0
sekim <sungeun.kim@zf.com>
2022-04-14 18:28:56 +0900

    validation logic

--- src/components/VariableCard.js
+++ src/components/VariableCard.js
@@ -13,59 +13,71 @@ export default function BasicCard() {
   const [values, setValues] = useState({
     sheet: "Sheet1",
     cell: "A35",
+    start: "",
+    end: "",
+    step: "",
+  })
+
+  const [valuesAreNum, setValuesAreNum] = useState({
     start: null,
     end: null,
     step: null,
   })
 
-  const [valueIsNaN, setValueIsNaN] = useState({
-    start: null,
-    end: null,
-    step: null,
-  })
+  const [endGreaterStart, setEndGreaterStart] = useState()
+  const [stepAboveZero, setStepAboveZero] = useState()
 
   useEffect(() => {
-    return () => {
       if (isNaN(values.start)) {
-        setValueIsNaN((prevState) => {
+        setValuesAreNum((prevState) => {
           return { ...prevState, ['start']: false }
         })
       } else {
-        setValueIsNaN((prevState) => {
+        setValuesAreNum((prevState) => {
           return { ...prevState, ['start']: true }
         })
       }
-    }
   }, [values.start])
 
   useEffect(() => {
-    return () => {
       if (isNaN(values.end)) {
-        setValueIsNaN((prevState) => {
+        setValuesAreNum((prevState) => {
           return { ...prevState, ['end']: false }
         })
       } else {
-        setValueIsNaN((prevState) => {
+        setValuesAreNum((prevState) => {
           return { ...prevState, ['end']: true }
         })
       }
-    }
   }, [values.end])
 
   useEffect(() => {
-    return () => {
       if (isNaN(values.step)) {
-        setValueIsNaN((prevState) => {
+        setValuesAreNum((prevState) => {
           return { ...prevState, ['step']: false }
         })
       } else {
-        setValueIsNaN((prevState) => {
+        setValuesAreNum((prevState) => {
           return { ...prevState, ['step']: true }
         })
       }
-    }
   }, [values.step])
-  
+
+  useEffect(() => {
+    if (valuesAreNum.start && valuesAreNum.end) {
+      Number(values.end) > Number(values.start) ? setEndGreaterStart(true) : setEndGreaterStart(false)
+    } else {
+      setEndGreaterStart(null)
+    }
+  }, [values.start, values.end, valuesAreNum.start, valuesAreNum.end])
+
+  useEffect(() => {
+    if (valuesAreNum.step) {
+      Number(values.step) > 0 ? setStepAboveZero(true) : setStepAboveZero(false)
+    } else {
+      setStepAboveZero(null)
+    }
+  }, [values.step, valuesAreNum.step])
 
   const handleChange = (e) => {
     setValues((prevState) => {
@@ -80,39 +92,39 @@ export default function BasicCard() {
           {values.sheet}
         </Typography>
         <Typography variant="h5" component="div">
-        {values.cell}
+          {values.cell}
         </Typography>
         <Stack spacing={2} direction="row">
-        <FormControl size='small'>
-          <InputLabel htmlFor="component-outlined">Start</InputLabel>
-          <OutlinedInput
-            id="component-outlined-start"
-            label="Start"
-            name="start"
-            value={values.start}
-            onChange={handleChange}
-          />
-        </FormControl>
-        <FormControl size='small'>
-          <InputLabel htmlFor="component-outlined">End</InputLabel>
-          <OutlinedInput
-            id="component-outlined-end"
-            label="End"
-            name="end"
-            value={values.end}
-            onChange={handleChange}
-          />
-        </FormControl>
-        <FormControl size='small'>
-          <InputLabel htmlFor="component-outlined">Step</InputLabel>
-          <OutlinedInput
-            id="component-outlined-step"
-            label="Step"
-            name="step"
-            value={values.step}
-            onChange={handleChange}
-          />
-        </FormControl>
+          <FormControl size='small'>
+            <InputLabel htmlFor="component-outlined">Start</InputLabel>
+            <OutlinedInput
+              id="component-outlined-start"
+              label="Start"
+              name="start"
+              value={values.start}
+              onChange={handleChange}
+            />
+          </FormControl>
+          <FormControl size='small'>
+            <InputLabel htmlFor="component-outlined">End</InputLabel>
+            <OutlinedInput
+              id="component-outlined-end"
+              label="End"
+              name="end"
+              value={values.end}
+              onChange={handleChange}
+            />
+          </FormControl>
+          <FormControl size='small'>
+            <InputLabel htmlFor="component-outlined">Step</InputLabel>
+            <OutlinedInput
+              id="component-outlined-step"
+              label="Step"
+              name="step"
+              value={values.step}
+              onChange={handleChange}
+            />
+          </FormControl>
         </Stack>
       </CardContent>
       <CardActions>
