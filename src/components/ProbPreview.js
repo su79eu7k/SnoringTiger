37786f94910d493f380c9db93c32f88f673e8e55
su79eu7k <su79eu7k@gmail.com>
2022-05-04 00:50:35 +0900

    Chart config based on theme, legend hidden.

--- src/components/ProbPreview.js
+++ src/components/ProbPreview.js
@@ -1,4 +1,5 @@
 import React, { useRef, useEffect } from 'react'
+import { useTheme } from '@mui/styles';
 import Chart from "chart.js/auto";
 import _ from 'lodash'
 
@@ -8,7 +9,8 @@ export default React.memo(function PropPreview(props) {
   const coords = props.coords
   const cellTypeAuto = props.cellTypeAuto
   const canvasRef = useRef()
-
+  const theme = useTheme();
+ 
   useEffect(() => {
     const ctx = canvasRef.current.getContext("2d")
     const probChart = new Chart(ctx, {
@@ -16,8 +18,17 @@ export default React.memo(function PropPreview(props) {
       data: {
         labels: x.map(v => v.toFixed(4)),
         datasets: [{
-          data: cellTypeAuto ? coords : prob
+          data: cellTypeAuto ? coords : prob,
+          borderColor: theme.palette.text.secondary,
+          backgroundColor: theme.palette.text.secondary,
         }]
+      },
+      options: {
+        plugins: {
+          legend: {
+            display: false
+          }
+        }
       }
     })
 
