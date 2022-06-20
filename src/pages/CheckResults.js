d466523bbf2e1870de3341f23f3a4d243eeaa043
sekim <sungeun.kim@zf.com>
2022-06-20 10:56:59 +0900

    Init

--- src/pages/CheckResults.js
+++ src/pages/CheckResults.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import { DataGrid } from '@mui/x-data-grid';
 
 export default function CheckResults() {
   return (
-    <div>CheckResults</div>
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <Typography variant="h6">
+          Check Results
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Card sx={{ minWidth: 275 }}>
+          <CardContent>
+            card content
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
   )
 }
