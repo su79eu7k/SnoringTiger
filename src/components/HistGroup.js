f51187b9f33019517f6b8ed1a5846e49be22c575
su79eu7k <su79eu7k@gmail.com>
2022-06-25 00:15:55 +0900

    Minor.

--- src/components/HistGroup.js
+++ src/components/HistGroup.js
@@ -1,30 +1,12 @@
-import { useState, useEffect } from 'react';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Divider from '@mui/material/Divider';
 import Stack from '@mui/material/Stack';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import DeleteIcon from '@mui/icons-material/Delete';
 import FolderIcon from '@mui/icons-material/Folder';
-import IconButton from '@mui/material/IconButton';
-import PushPinIcon from '@mui/icons-material/PushPin';
-import FileDownloadIcon from '@mui/icons-material/FileDownload';
-import ExpandLess from '@mui/icons-material/ExpandLess';
-import ExpandMore from '@mui/icons-material/ExpandMore';
-import ListItemButton from '@mui/material/ListItemButton';
-import Collapse from '@mui/material/Collapse';
-import StarBorder from '@mui/icons-material/StarBorder';
 import SaveIcon from '@mui/icons-material/Save';
 import CameraAltIcon from '@mui/icons-material/CameraAlt';
-import BookmarkIcon from '@mui/icons-material/Bookmark';
-import axios from 'axios';
-import _ from 'lodash'
 import { DateTime } from "luxon";
 import ControlButton from './ControlButton';
 
