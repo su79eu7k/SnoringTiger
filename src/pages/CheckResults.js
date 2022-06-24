baf0acacaaf8b60acb909ae40c2c709d23fd9020
sekim <sungeun.kim@zf.com>
2022-06-24 14:12:03 +0900

    Init

--- src/pages/CheckResults.js
+++ src/pages/CheckResults.js
@@ -1,22 +1,72 @@
+import { useState, useEffect } from 'react';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import { DataGrid } from '@mui/x-data-grid';
+import Divider from '@mui/material/Divider';
+import Stack from '@mui/material/Stack';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import DeleteIcon from '@mui/icons-material/Delete';
+import FolderIcon from '@mui/icons-material/Folder';
+import IconButton from '@mui/material/IconButton';
+import PushPinIcon from '@mui/icons-material/PushPin';
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+import ListItemButton from '@mui/material/ListItemButton';
+import Collapse from '@mui/material/Collapse';
+import StarBorder from '@mui/icons-material/StarBorder';
+import CameraAltIcon from '@mui/icons-material/CameraAlt';
 
 export default function CheckResults() {
+  const [open, setOpen] = useState(true);
+
+  const handleClickExpand = () => {
+    setOpen(!open);
+  };
+
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}>
         <Typography variant="h6">
-          Check Results
+          History
         </Typography>
       </Grid>
       <Grid item xs={12}>
         <Card sx={{ minWidth: 275 }}>
           <CardContent>
-            card content
+            <Stack direction="row">
+              <Typography noWrap variant="subtitle2" color="text.secondary" sx={{ padding: '3px 4px' }}>
+                Recent
+              </Typography>
+              <Typography noWrap variant="subtitle2" color="text.secondary" sx={{ padding: '3px 4px' }}>
+                Pinned
+              </Typography>
+            </Stack>
+            <List>
+              <Divider />
+              <ListItemButton onClick={handleClickExpand}>
+                <ListItemIcon>
+                  <FolderIcon />
+                </ListItemIcon>
+                <ListItemText primary="Inbox" />
+                {open ? <ExpandLess /> : <ExpandMore />}
+              </ListItemButton>
+              <Collapse in={open} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding dense>
+                  <ListItemButton sx={{ pl: 4 }}>
+                    <ListItemIcon>
+                      <CameraAltIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Starred" />
+                  </ListItemButton>
+                </List>
+              </Collapse>
+            </List>
           </CardContent>
         </Card>
       </Grid>
