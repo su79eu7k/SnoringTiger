bd6cb09ddf4fbd4b7ca1f9944b18ef52165e6e48
sekim <sungeun.kim@zf.com>
2022-07-07 16:40:22 +0900

    WIP: test env build up

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from "@testing-library/user-event";
 import App from './App';
-import fs from "fs";
-import { Blob } from "buffer";
 
 
 describe('app before upload file', () => {
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -2,36 +2,35 @@
 import { rest } from 'msw'
 
 export const handlers = [
-  rest.post('/login', (req, res, ctx) => {
-    // Persist user's authentication in the session
-    sessionStorage.setItem('is-authenticated', 'true')
-
+  rest.get('http://127.0.0.1:8000/check_connection', (req, res, ctx) => {
     return res(
-      // Respond with a 200 status code
       ctx.status(200),
+      ctx.json(
+        {code: 1, message: 'test.xlsx'}
+      )
     )
   }),
 
-  rest.get('/user', (req, res, ctx) => {
-    // Check if the user is authenticated in this session
-    const isAuthenticated = sessionStorage.getItem('is-authenticated')
-
-    if (!isAuthenticated) {
-      // If not authenticated, respond with a 403 error
-      return res(
-        ctx.status(403),
-        ctx.json({
-          errorMessage: 'Not authorized',
-        }),
-      )
-    }
-
-    // If authenticated, return a mocked user details
+  rest.get('http://127.0.0.1:8000/reset', (req, res, ctx) => {
     return res(
       ctx.status(200),
-      ctx.json({
-        username: 'admin',
-      }),
+      ctx.json(
+        {code: 1, message: 'Success: Session re-initiated.'}
+      )
+    )
+  }),
+
+  rest.get('http://127.0.0.1:8000/get_selection', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json(
+        {
+          sheet: "Sheet1",
+          range: "A1",
+          code: 1,
+          message: "Success: Connection, Getting selection."
+        }
+      )
     )
   }),
 ]
