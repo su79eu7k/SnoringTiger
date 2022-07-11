82a4d983ea379546336bd27886db1d27758d3b08
sekim <sungeun.kim@zf.com>
2022-07-11 16:58:10 +0900

    API call for get_params_detail with hash_params.

--- src/components/Report.js
+++ src/components/Report.js
@@ -1,14 +1,43 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
+import { DateTime } from "luxon";
+import axios from 'axios';
 
 function Report(props) {
   const openReportModal = props.openReportModal
   const setOpenReportModal = props.setOpenReportModal
   const filename = props.filename
   const hash_params = props.hash_params
+
+  const [paramsDetail, setParamsDetail] = useState()
+  const [scopedData, setScopedData] = useState()
+  const [loading, setLoading] = useState({
+    params_detail: false,
+    scoped_data: false,
+  })
+  const [lastUpdated, setLastUpdated] = useState(DateTime.now().toUnixInteger())
+
+  useEffect(() => {
+    setLoading(prevState => ({...prevState, 'params_detail': true}))
+    axios.get("http://127.0.0.1:8000/get_params_detail?hash_params=" + hash_params).then((response) => {
+      setParamsDetail(response.data)
+      setLoading(prevState => ({...prevState, 'params_detail': false}))
+    }).catch(() => {})
+
+    // setLoading(prevState => ({...prevState, 'scoped_data': true}))
+    // axios.get("http://127.0.0.1:8000/get_scoped_data").then((response) => {
+    //   setScopedData(response.data)
+    //   setLoading(prevState => ({...prevState, 'scoped_data': false}))
+    // }).catch(() => {})
+  }, [lastUpdated])
+  
+  useEffect(() => {
+    console.log(paramsDetail)
+  }, [paramsDetail])
+  
 
   return (
     <Modal
